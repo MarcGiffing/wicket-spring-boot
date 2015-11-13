8b335000d886b71308bf8a357e414e3eeed7aef4
Marc Giffing <marcgiffing@web.de>
2015-11-13 23:24:06 +0100

    added possibility to change values of the HtmlCompressor class dynamically over the property configuration. It will uses reflection to set boolean flags on public methods. (Maybe not usable for all use cases but enough for simple configurations) e.g.: wicket.wicketstuff.htmlcompressor.properties.compressCss=true

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/wicketstuff/htmlcompressor/HTMLCompressingConfig.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/wicketstuff/htmlcompressor/HTMLCompressingConfig.java
@@ -1,14 +1,22 @@
 package com.giffing.wicket.spring.boot.starter.configuration.extensions.wicketstuff.htmlcompressor;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map.Entry;
+
 import org.apache.wicket.protocol.http.WebApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
 import org.wicketstuff.htmlcompressor.HtmlCompressingMarkupFactory;
 
 import com.giffing.wicket.spring.boot.starter.configuration.WicketApplicationInitConfiguration;
+import com.googlecode.htmlcompressor.compressor.Compressor;
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
 
 /**
  * Enables the HTML markup compression if the following two conditions matches:
@@ -32,6 +40,18 @@ public class HTMLCompressingConfig implements WicketApplicationInitConfiguration
 
 	@Override
 	public void init(WebApplication webApplication) {
-		webApplication.getMarkupSettings().setMarkupFactory(new HtmlCompressingMarkupFactory());
+		HtmlCompressor compressor = new HtmlCompressor();
+		for(Entry<String, Boolean> entrySet : props.getProperties().entrySet()){
+			Method method;
+			try {
+				String capitalizedKey = StringUtils.capitalize(entrySet.getKey());
+				method = compressor.getClass().getMethod("set" + capitalizedKey, boolean.class);
+				method.setAccessible(true);
+			} catch (NoSuchMethodException e) {
+				throw new IllegalStateException(e);
+			}
+			ReflectionUtils.invokeMethod(method, compressor, entrySet.getValue());
+		}
+		webApplication.getMarkupSettings().setMarkupFactory(new HtmlCompressingMarkupFactory(compressor));
 	}
 }
--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/wicketstuff/htmlcompressor/HTMLCompressingProperties.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/wicketstuff/htmlcompressor/HTMLCompressingProperties.java
@@ -1,6 +1,11 @@
 package com.giffing.wicket.spring.boot.starter.configuration.extensions.wicketstuff.htmlcompressor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
 
 @ConfigurationProperties(prefix = "wicket.wicketstuff.htmlcompressor")
 public class HTMLCompressingProperties {
@@ -12,6 +17,11 @@ public class HTMLCompressingProperties {
 	 * @see HTMLCompressingConfig
 	 */
 	private boolean enabled = true;
+	
+	/**
+	 * Additional native properties to set on the {@link HtmlCompressor} provider.
+	 */
+	private Map<String, Boolean> properties = new HashMap<String, Boolean>();
 
 	public boolean isEnabled() {
 		return enabled;
@@ -19,6 +29,14 @@ public class HTMLCompressingProperties {
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	public Map<String, Boolean> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, Boolean> properties) {
+		this.properties = properties;
 	}
 
 }
