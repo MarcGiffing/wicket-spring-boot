97b811f4e2bc9e093898226efe4292e923bcdd63
Marc Giffing <marcgiffing@web.de>
2015-11-14 10:18:12 +0100

    extension - devutils: added disk store manager which can be enabled over property/yml configuration. It's ovverides the default disk store manager. It is mounted by default to devutils/diskstore/browser (overwritable)

---
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/devutils/diskstorebrowser/DiskStoreBrowserConfig.java
@@ -0,0 +1,30 @@
+package com.giffing.wicket.spring.boot.starter.configuration.extensions.devutils.diskstorebrowser;
+
+import org.apache.wicket.devutils.diskstore.DebugPageManagerProvider;
+import org.apache.wicket.devutils.diskstore.DiskStoreBrowserPage;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import com.giffing.wicket.spring.boot.starter.configuration.WicketApplicationInitConfiguration;
+
+@Component
+@ConditionalOnProperty(prefix = "wicket.devutils.diskstorebrowser", value = "enabled", matchIfMissing = false)
+@ConditionalOnClass(value = org.apache.wicket.devutils.diskstore.DiskStoreBrowserPage.class)
+@EnableConfigurationProperties({ DiskStoreBrowserProperties.class })
+public class DiskStoreBrowserConfig implements WicketApplicationInitConfiguration {
+
+	@Autowired
+	private DiskStoreBrowserProperties properties;
+	
+	@Override
+	public void init(WebApplication webApplication) {
+		DebugPageManagerProvider pageManager = new DebugPageManagerProvider(webApplication);
+		webApplication.setPageManagerProvider(pageManager);
+		webApplication.mountPage(properties.getMountPage(), DiskStoreBrowserPage.class);
+	}
+
+}
