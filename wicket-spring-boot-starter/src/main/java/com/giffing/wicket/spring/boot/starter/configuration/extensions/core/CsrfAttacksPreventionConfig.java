5ab8cc0193dad3cbb6eb5700c2ce5866be80f93b
Marc Giffing <marcgiffing@web.de>
2015-11-15 08:48:06 +0100

    close #4 - fixed Autoconfigure for CsrfPreventionRequestCycleListener

---
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/CsrfAttacksPreventionConfig.java
@@ -0,0 +1,38 @@
+package com.giffing.wicket.spring.boot.starter.configuration.extensions.core;
+
+import org.apache.wicket.bean.validation.BeanValidationConfiguration;
+import org.apache.wicket.protocol.http.CsrfPreventionRequestCycleListener;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import com.giffing.wicket.spring.boot.starter.configuration.WicketApplicationInitConfiguration;
+
+
+/**
+ * Enables CSRF protection if the following condition matches.
+ * 
+ * 1. The {@link CsrfPreventionRequestCycleListener} class is in the classpath.
+ * 
+ * 2. The property {@link CsrfAttacksPreventionProperties#PROPERTY_PREFIX}.enabled has to be true (default = true)
+ *
+ * The protection should be enabled by default cause the {@link CsrfPreventionRequestCycleListener} is located
+ * in Wickets core project.
+ * 
+ * @author Marc Giffing
+ *
+ */
+@Component
+@ConditionalOnProperty(prefix = CsrfAttacksPreventionProperties.PROPERTY_PREFIX, value = "enabled", matchIfMissing = true)
+@ConditionalOnClass(value = org.apache.wicket.protocol.http.CsrfPreventionRequestCycleListener.class)
+@EnableConfigurationProperties({ CsrfAttacksPreventionProperties.class })
+public class CsrfAttacksPreventionConfig implements WicketApplicationInitConfiguration{
+
+	@Override
+	public void init(WebApplication webApplication) {
+		webApplication.getRequestCycleListeners().add(new CsrfPreventionRequestCycleListener());
+	}
+
+}
---
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/CsrfAttacksPreventionProperties.java
@@ -0,0 +1,23 @@
+package com.giffing.wicket.spring.boot.starter.configuration.extensions.core;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = CsrfAttacksPreventionProperties.PROPERTY_PREFIX)
+public class CsrfAttacksPreventionProperties {
+	
+	public static  final String PROPERTY_PREFIX = "wicket.core.csrf";
+	
+	/**
+	 * Enables Wickets CSRF protection
+	 */
+	private boolean enabled = true;
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
