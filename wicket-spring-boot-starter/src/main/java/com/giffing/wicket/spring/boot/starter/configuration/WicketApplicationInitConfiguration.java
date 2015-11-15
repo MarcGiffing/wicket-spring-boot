878ea9b1557f66ff579941fd00e3846eceee9676
Marc Giffing <marcgiffing@web.de>
2015-11-15 12:37:37 +0100

    javadoc

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/WicketApplicationInitConfiguration.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/WicketApplicationInitConfiguration.java
@@ -2,10 +2,13 @@ package com.giffing.wicket.spring.boot.starter.configuration;
 
 import org.apache.wicket.protocol.http.WebApplication;
 
+import com.giffing.wicket.spring.boot.starter.app.WicketBootWebApplication;
+
 /**
- * 
- * Spring configuration extensions should implement this interface to enable
- * wicket specific configuration wich should be executed in the init method.
+ * To provide custom modification of the init Method of Wickets {@link WebApplication}
+ * class. An extension class should implement this interface. All classes implementing
+ * this interface are injected in the {@link WicketBootWebApplication} as a list and on
+ * each implementation the init method is called with the current {@link WebApplication}.
  * 
  * @author Marc Giffing
  *
