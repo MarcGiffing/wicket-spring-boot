fcef3edfe7ad96678c42355ce8f53f04bf4cc0c2
Marc Giffing <marcgiffing@web.de>
2015-11-19 21:01:43 +0100

    formatting

--- wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/WicketApplication.java
+++ wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/WicketApplication.java
@@ -7,11 +7,11 @@ import com.giffing.wicket.spring.boot.starter.context.WicketSpringBootApplicatio
 
 @WicketSpringBootApplication
 public class WicketApplication extends WicketBootWebApplication {
-
+	
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder()
 			.sources(WicketApplication.class)
 			.run(args);
 	}
-
+	
 }
