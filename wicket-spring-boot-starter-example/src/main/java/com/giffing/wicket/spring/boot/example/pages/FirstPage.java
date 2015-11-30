5a9610c2575181f2006d54997ac1537034a042b6
Marc Giffing <marcgiffing@web.de>
2015-11-30 21:23:42 +0100

    added example page to see that @MountPath works

---
+++ wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/pages/FirstPage.java
@@ -0,0 +1,9 @@
+package com.giffing.wicket.spring.boot.example.pages;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@MountPath("firstPage")
+public class FirstPage extends WebPage {
+
+}
