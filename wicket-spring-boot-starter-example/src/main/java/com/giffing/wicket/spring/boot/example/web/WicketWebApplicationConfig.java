8e2c998eb7ce656583d627af0b4c7661194a4526
Marc Giffing <marcgiffing@web.de>
2015-12-23 09:09:21 +0100

    example - test performance - if we only want to test Wicket with mocks we don't need a database. (further improvements needed)

--- wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/web/WicketWebApplicationConfig.java
+++ wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/web/WicketWebApplicationConfig.java
@@ -1,9 +1,22 @@
 package com.giffing.wicket.spring.boot.example.web;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+
 import com.giffing.wicket.spring.boot.starter.app.WicketBootWebApplication;
 import com.giffing.wicket.spring.boot.starter.context.WicketSpringBootApplication;
 
+//TODO move to test
 @WicketSpringBootApplication
+@EnableAutoConfiguration(exclude={
+		DataSourceAutoConfiguration.class, 
+		JpaRepositoriesAutoConfiguration.class, 
+		JpaBaseConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+	})
 public class WicketWebApplicationConfig extends WicketBootWebApplication {
 
 }
