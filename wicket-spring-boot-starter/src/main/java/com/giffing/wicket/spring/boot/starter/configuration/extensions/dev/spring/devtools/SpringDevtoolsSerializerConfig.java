dd59d829ee8c9d12228435044adaf229e6582526
Marc Giffing <marcgiffing@web.de>
2015-12-27 14:46:50 +0100

    Back Button not working with Spring Boot devtools #29
    documentation

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/dev/spring/devtools/SpringDevtoolsSerializerConfig.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/dev/spring/devtools/SpringDevtoolsSerializerConfig.java
@@ -9,6 +9,19 @@ import org.springframework.boot.devtools.restart.ConditionalOnInitializedRestart
 import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
 import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
 
+/**
+ * The Wicket serializer does not working with Spring Boot Devtools so we have to provide a custom {@link SpringDevToolsSerializer}.
+ * It should be active by default if the following conditions matches.
+ * 
+ * 1. The {@link LocalDevToolsAutoConfiguration} class is in the classpath. This means that the Spring Boot Devtools is available in the classpath.
+ * 
+ * 2. The property "spring.devtools.restart.enabled" is set to true. (default is true). There is maybe a better option to check
+ * 
+ * 3. Restarter condition is active {@link ConditionalOnInitializedRestarter}.
+ * 
+ * @author Marc Giffing
+ *
+ */
 @ApplicationInitExtension
 @ConditionalOnProperty(prefix = "spring.devtools.restart", value = "enabled", matchIfMissing = true)
 @ConditionalOnClass(LocalDevToolsAutoConfiguration.class)
