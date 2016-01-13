108d22e23c497c5f952465fb8e9235bde3a66d0b
Marc Giffing <marcgiffing@web.de>
2016-01-13 10:51:04 +0100

    Prefer BookmarkablePageLink when the only line in the body is setResponsePage(Class).

--- wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/web/pages/home/HomePage.java
+++ wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/web/pages/home/HomePage.java
@@ -1,7 +1,7 @@
 package com.giffing.wicket.spring.boot.example.web.pages.home;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import com.giffing.wicket.spring.boot.example.web.pages.BasePage;
@@ -12,14 +12,7 @@ import com.giffing.wicket.spring.boot.example.web.pages.customers.CustomerListPa
 public class HomePage extends BasePage {
 
 	public HomePage(){
-		add(new Link<String>("customersLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(CustomerListPage.class);
-				
-			}
-		});
+		add(new BookmarkablePageLink<String>("customersLink", CustomerListPage.class));
 	}
 	
 }
