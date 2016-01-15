87627b62752c1d55f29fe7da67df0cf5e972a33b
Marc Giffing <marcgiffing@web.de>
2016-01-15 17:40:31 +0100

    enhanced csrf prevention property configuration

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/csrf/CsrfAttacksPreventionConfig.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/csrf/CsrfAttacksPreventionConfig.java
@@ -2,6 +2,7 @@ package com.giffing.wicket.spring.boot.starter.configuration.extensions.core.csr
 
 import org.apache.wicket.protocol.http.CsrfPreventionRequestCycleListener;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -29,9 +30,20 @@ import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitCo
 @EnableConfigurationProperties({ CsrfAttacksPreventionProperties.class })
 public class CsrfAttacksPreventionConfig implements WicketApplicationInitConfiguration{
 
+	@Autowired
+	private CsrfAttacksPreventionProperties properties;
+	
 	@Override
 	public void init(WebApplication webApplication) {
-		webApplication.getRequestCycleListeners().add(new CsrfPreventionRequestCycleListener());
+		CsrfPreventionRequestCycleListener listener = new CsrfPreventionRequestCycleListener();
+		listener.setConflictingOriginAction(properties.getConflictingOriginAction());
+		listener.setErrorCode(properties.getErrorCode());
+		listener.setErrorMessage(properties.getErrorMessage());
+		listener.setNoOriginAction(properties.getNoOriginAction());
+		for (String acceptedOrigin : properties.getAcceptedOrigins()) {
+			listener.addAcceptedOrigin(acceptedOrigin);
+		}
+		webApplication.getRequestCycleListeners().add(listener);
 	}
 
 }
--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/csrf/CsrfAttacksPreventionProperties.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/csrf/CsrfAttacksPreventionProperties.java
@@ -1,11 +1,43 @@
 package com.giffing.wicket.spring.boot.starter.configuration.extensions.core.csrf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.protocol.http.CsrfPreventionRequestCycleListener.CsrfAction;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = CsrfAttacksPreventionProperties.PROPERTY_PREFIX)
 public class CsrfAttacksPreventionProperties {
 	
 	public static final String PROPERTY_PREFIX = "wicket.core.csrf";
+	
+	/**
+	 * Action to perform when no Origin header is present in the request.
+	 */
+	private CsrfAction noOriginAction = CsrfAction.ALLOW;
+
+	/**
+	 * Action to perform when a conflicting Origin header is found.
+	 */
+	private CsrfAction conflictingOriginAction = CsrfAction.ABORT;
+
+	/**
+	 * The error code to report when the action to take for a CSRF request is
+	 * {@link CsrfAction#ABORT}. Default {@code 400 BAD REQUEST}.
+	 */
+	private int errorCode = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+
+	/**
+	 * The error message to report when the action to take for a CSRF request is {@code ERROR}.
+	 * Default {@code "Origin does not correspond to request"}.
+	 */
+	private String errorMessage = "Origin does not correspond to request";
+
+	/**
+	 * A white list of accepted origins (host names/domain names) presented as
+	 * &lt;domainname&gt;.&lt;TLD&gt;. The domain part can contain subdomains.
+	 */
+	private List<String> acceptedOrigins = new ArrayList<>();
 	
 	/**
 	 * Enables Wickets CSRF protection
@@ -18,6 +50,46 @@ public class CsrfAttacksPreventionProperties {
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	public CsrfAction getNoOriginAction() {
+		return noOriginAction;
+	}
+
+	public void setNoOriginAction(CsrfAction noOriginAction) {
+		this.noOriginAction = noOriginAction;
+	}
+
+	public CsrfAction getConflictingOriginAction() {
+		return conflictingOriginAction;
+	}
+
+	public void setConflictingOriginAction(CsrfAction conflictingOriginAction) {
+		this.conflictingOriginAction = conflictingOriginAction;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public List<String> getAcceptedOrigins() {
+		return acceptedOrigins;
+	}
+
+	public void setAcceptedOrigins(List<String> acceptedOrigins) {
+		this.acceptedOrigins = acceptedOrigins;
 	}
 
 }
