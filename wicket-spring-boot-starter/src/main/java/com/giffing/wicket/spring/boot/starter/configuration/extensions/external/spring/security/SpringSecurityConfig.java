eac0de07d93854ee8d8a22ad3d3e8269c76d6948
Marc Giffing <marcgiffing@web.de>
2016-03-16 17:16:29 +0100

    Introduce Special Annotations to mark SignInPage and HomePage #66
    configure WicketBootStandardWebApplication only if bean
    WicketBootWebApplication is missing

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/external/spring/security/SpringSecurityConfig.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/external/spring/security/SpringSecurityConfig.java
@@ -1,24 +1,17 @@
 package com.giffing.wicket.spring.boot.starter.configuration.extensions.external.spring.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
-import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
 import com.giffing.wicket.spring.boot.context.security.AuthenticatedWebSessionConfig;
 import com.giffing.wicket.spring.boot.starter.app.WicketBootSecuredWebApplication;
 import com.giffing.wicket.spring.boot.starter.app.WicketBootWebApplication;
-import com.giffing.wicket.spring.boot.starter.configuration.extensions.core.settings.general.GeneralSettingsProperties;
 
 @ApplicationInitExtension
 @ConditionalOnProperty(prefix = SpringSecurityProperties.PROPERTY_PREFIX, value = "enabled", matchIfMissing=true)
@@ -30,15 +23,6 @@ import com.giffing.wicket.spring.boot.starter.configuration.extensions.core.sett
 @EnableConfigurationProperties({ SpringSecurityProperties.class })
 public class SpringSecurityConfig {
 	
-	@Autowired
-	private static ApplicationContext applicationContext;
-
-	@Autowired
-	private static GeneralSettingsProperties generalSettingsProperties;
-
-	@Autowired(required = false)
-	private static List<WicketApplicationInitConfiguration> configurations = new ArrayList<>();
-	
 	
 	@Configuration
 	@ConditionalOnMissingBean(WicketBootWebApplication.class)
@@ -46,11 +30,7 @@ public class SpringSecurityConfig {
 		
 		@Bean
 		public WicketBootSecuredWebApplication wicketBootWebApplication() {
-			WicketBootSecuredWebApplication application = new WicketBootSecuredWebApplication();
-			application.setApplicationContext(applicationContext);
-			application.setConfigurations(configurations);
-			application.setGeneralSettingsProperties(generalSettingsProperties);
-			return application;
+			return new WicketBootSecuredWebApplication();
 		}
 		
 	}
