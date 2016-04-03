85c5d2284186f2f102093459a6cc5199fdb09576
Marc Giffing <marcgiffing@web.de>
2016-04-03 10:44:41 +0200

    documentation

--- wicket-spring-boot-context/src/main/java/com/giffing/wicket/spring/boot/context/scan/WicketInternalErrorPage.java
+++ wicket-spring-boot-context/src/main/java/com/giffing/wicket/spring/boot/context/scan/WicketInternalErrorPage.java
@@ -6,6 +6,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.apache.wicket.Page;
+import org.apache.wicket.settings.ApplicationSettings;
+
+/**
+ * This annotation can be used to mark a {@link Page} which will automatically configured
+ * as the internal error page in the {@link ApplicationSettings}.
+ * 
+ * @author Marc Giffing
+ *
+ */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
