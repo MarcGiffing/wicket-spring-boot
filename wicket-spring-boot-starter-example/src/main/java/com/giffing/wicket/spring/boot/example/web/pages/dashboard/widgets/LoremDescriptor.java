405229f72474afb3008541358676817de5a17243
marcgiffing <marcgiffing@web.de>
2016-09-23 21:24:56 +0200

    Autoconfiguration - Wicketstuff - Dashboard #80
    removed dashboard config

--- wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/web/pages/dashboard/widgets/LoremDescriptor.java
+++
@@ -1,9 +0,0 @@
-package com.giffing.wicket.spring.boot.example.web.pages.dashboard.widgets;
-
-import org.springframework.stereotype.Component;
-import org.wicketstuff.dashboard.widgets.loremipsum.LoremIpsumWidgetDescriptor;
-
-@Component
-public class LoremDescriptor extends LoremIpsumWidgetDescriptor {
-
-}
