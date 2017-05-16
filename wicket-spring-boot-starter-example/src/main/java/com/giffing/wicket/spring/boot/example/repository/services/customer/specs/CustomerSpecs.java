202f30dc4e3a23d66b95983b501bd1fa09ae0c98
marcgiffing <marcgiffing@web.de>
2017-05-16 21:59:30 +0200

    use lambda expressions for specifications

--- wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/repository/services/customer/specs/CustomerSpecs.java
+++ wicket-spring-boot-starter-example/src/main/java/com/giffing/wicket/spring/boot/example/repository/services/customer/specs/CustomerSpecs.java
@@ -13,50 +13,26 @@ import com.giffing.wicket.spring.boot.example.model.Customer_;
 public class CustomerSpecs {
 
 	public static Specification<Customer> hasId(final Long id) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.equal(root.get(Customer_.id), id);
-			}
-		};
+		return (root, query, builder) -> builder.equal(root.get(Customer_.id), id);
 	}
 
 	public static Specification<Customer> hasUsername(final String username) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.equal(builder.lower(root.get(Customer_.username)), username.toLowerCase());
-			}
-		};
+		return (root, query, builder) -> builder.equal(builder.lower(root.get(Customer_.username)), username.toLowerCase());
 	}
 
 	public static Specification<Customer> hasUsernameLike(final String username) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.like(builder.lower(root.get(Customer_.username)), "%" + username.toLowerCase() + "%");
-			}
-		};
+		return (root, query, builder) -> builder.like(builder.lower(root.get(Customer_.username)), "%" + username.toLowerCase() + "%");
 	}
 
 	public static Specification<Customer> hasFirstnameLike(final String firstname) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.like(builder.lower(root.get(Customer_.firstname)), "%" + firstname.toLowerCase() + "%");
-			}
-		};
+		return (root, query, builder) -> builder.like(builder.lower(root.get(Customer_.firstname)), "%" + firstname.toLowerCase() + "%");
 	}
 
 	public static Specification<Customer> hasLastnameLike(final String lastname) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.like(builder.lower(root.get(Customer_.lastname)), "%" + lastname.toLowerCase() + "%");
-			}
-		};
+		return (root, query, builder) -> builder.like(builder.lower(root.get(Customer_.lastname)), "%" + lastname.toLowerCase() + "%");
 	}
 
 	public static Specification<Customer> hasActive(final boolean active) {
-		return new Specification<Customer>() {
-			public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-				return builder.equal(root.get(Customer_.active), active);
-			}
-		};
+		return (root, query, builder) -> builder.equal(root.get(Customer_.active), active);
 	}
 }
