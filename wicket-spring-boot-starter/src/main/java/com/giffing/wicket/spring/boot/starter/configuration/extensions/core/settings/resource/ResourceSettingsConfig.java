1bf711d348ea12a867952ac661029a3e2e376fc7
Tobias Gierke <tobias.gierke@voipfuture.com>
2020-01-24 17:24:28 +0100

    support properties for line-precise error reporting,resource polling frequency and whether to use minified resources

---
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/core/settings/resource/ResourceSettingsConfig.java
@@ -0,0 +1,27 @@
+package com.giffing.wicket.spring.boot.starter.configuration.extensions.core.settings.resource;
+
+import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtension;
+import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.util.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@ApplicationInitExtension
+@ConditionalOnProperty(prefix = ResourceSettingsProperties.PROPERTY_PREFIX, value = "enabled", matchIfMissing = false)
+@EnableConfigurationProperties({ ResourceSettingsProperties.class })
+public class ResourceSettingsConfig implements WicketApplicationInitConfiguration
+{
+    @Autowired
+	private ResourceSettingsProperties properties;
+
+    @Override
+    public void init(WebApplication webApplication)
+    {
+        webApplication.getResourceSettings().setUseMinifiedResources(properties.isUseMinifiedResources());
+        if (properties.getResourcePollFrequencySeconds() > 0) {
+            webApplication.getResourceSettings().setResourcePollFrequency(Duration.seconds( properties.getResourcePollFrequencySeconds()));
+        }
+    }
+}
