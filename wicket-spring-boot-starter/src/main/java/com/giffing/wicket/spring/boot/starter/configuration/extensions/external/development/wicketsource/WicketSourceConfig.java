1c3cf98ece040779f8aff3e2ac5bbc88947884ea
Marc Giffing <marcgiffing@web.de>
2021-12-18 21:03:33 +0100

    #184 readd wicket-source in version 9.0.0

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/external/development/wicketsource/WicketSourceConfig.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/configuration/extensions/external/development/wicketsource/WicketSourceConfig.java
@@ -2,6 +2,7 @@ package com.giffing.wicket.spring.boot.starter.configuration.extensions.external
 
 import org.apache.wicket.protocol.http.WebApplication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
@@ -9,6 +10,7 @@ import com.giffing.wicket.spring.boot.context.extensions.ApplicationInitExtensio
 import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
 import com.giffing.wicket.spring.boot.context.extensions.boot.actuator.WicketAutoConfig;
 import com.giffing.wicket.spring.boot.context.extensions.boot.actuator.WicketEndpointRepository;
+import com.github.jennybrown8.wicketsource.WicketSource;
 
 /**
  * Enables wicket-source support if the following two condition matches:
@@ -22,7 +24,7 @@ import com.giffing.wicket.spring.boot.context.extensions.boot.actuator.WicketEnd
  */
 @ApplicationInitExtension
 @ConditionalOnProperty(prefix = WicketSourceProperties.PROPERTY_PREFIX, value = "enabled", matchIfMissing = true)
-//@ConditionalOnClass(value = com.github.jennybrown8.wicketsource.WicketSource.class)
+@ConditionalOnClass(value = com.github.jennybrown8.wicketsource.WicketSource.class)
 @EnableConfigurationProperties({ WicketSourceProperties.class })
 public class WicketSourceConfig implements WicketApplicationInitConfiguration{
 
@@ -34,7 +36,7 @@ public class WicketSourceConfig implements WicketApplicationInitConfiguration{
 	
 	@Override
 	public void init(WebApplication webApplication) {
-		//WicketSource.configure(webApplication);
+		WicketSource.configure(webApplication);
 		
 		wicketEndpointRepository.add(new WicketAutoConfig.Builder(this.getClass())
 				.withDetail("properties", props)
