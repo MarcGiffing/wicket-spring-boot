6546b3ee2d5afee1f0e2a06cb761c8fc00f66871
hannesb <hannes.becker@proxora.com>
2022-10-22 00:08:25 +0200

    Make WicketWebInitializer more customizable to provide Payara compatibility

--- wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/web/WicketWebInitializer.java
+++ wicket-spring-boot-starter/src/main/java/com/giffing/wicket/spring/boot/starter/web/WicketWebInitializer.java
@@ -62,7 +62,7 @@ public class WicketWebInitializer implements ServletContextInitializer {
 		filter.setInitParameter("applicationBean", beanNamesForType[0]);
 
 		filter.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, props.getFilterMappingParam());
-		filter.addMappingForUrlPatterns(EnumSet.copyOf( props.getDispatcherTypes() ), false, props.getFilterMappingParam());
+		filter.addMappingForUrlPatterns(EnumSet.copyOf( props.getDispatcherTypes() ), props.isFilterMatchAfter(), props.getFilterMappingParam());
 
 		Map<String, String> initParameters = props.getInitParameters();
 		for (Entry<String, String> initParam : initParameters.entrySet()) {
